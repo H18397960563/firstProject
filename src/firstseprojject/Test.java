07ff9aee8a33cea28ed978ac982968ccc4252f51
SunFlower <SunFlower@SunFlower>
2020-04-16 20:24:35 +0800

    第一次提交

---
+++ src/firstseprojject/Test.java
@@ -0,0 +1,13 @@
+package firstseprojject;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+	
+		System.out.println("第一个人的第一行");
+		
+		System.out.println("第一个人的第二行");
+		
+	}
+}
